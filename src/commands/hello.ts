a6ed76194dcbfcb4e1a71d43de1babb798fbb6cf
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-09-01 23:45:10 +0200

    a

--- src/commands/hello.ts
+++ src/commands/hello.ts
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import { flags } from '@oclif/command'
 import { Command } from '../base'
+=======
+import {Command, flags} from '@oclif/command'
+
+>>>>>>> parent of b1c9c75... fix plugins 2 and format hello.ts
 export default class Hello extends Command {
   static description = 'describe the command here'
 
@@ -10,17 +15,17 @@ hello world from ./src/hello.ts!
   ]
 
   static flags = {
-    help: flags.help({ char: 'h' }),
+    help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name to print' }),
+    name: flags.string({char: 'n', description: 'name to print'}),
     // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
+    force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{ name: 'file' }]
+  static args = [{name: 'file'}]
 
   async run() {
-    const { args, flags } = this.parse(Hello)
+    const {args, flags} = this.parse(Hello)
 
     const name = flags.name ?? 'world'
     this.log(`hello ${name} from ./src/commands/hello.ts`)
