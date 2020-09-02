34a4531531613048c0a98641ab2eefd7a2c0020d
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-09-02 14:38:19 +0200

    remove unnesccary lines

--- src/commands/deployments/create.ts
+++ src/commands/deployments/create.ts
@@ -9,22 +9,13 @@ export default class CreateDeployment extends Command {
   ]
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    help: flags.help({char: 'h'})
   }
 
   static args = [{name: 'file'}]
 
   async run() {
     const {args, flags} = this.parse(CreateDeployment)
-
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/commands/hello.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    
   }
 }
--- src/commands/machines/delete.ts
+++ src/commands/machines/delete.ts
@@ -10,10 +10,6 @@ export default class DeleteMachine extends Command {
 
   static flags = {
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   }
 
   static args = [{name: 'file'}]
@@ -21,10 +17,5 @@ export default class DeleteMachine extends Command {
   async run() {
     const {args, flags} = this.parse(DeleteMachine)
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from ./src/commands/hello.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
   }
 }
